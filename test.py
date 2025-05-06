964fe1031e81b15c446777606a8a44592b1d3dfe
sam15211521 <70865013+sam15211521@users.noreply.github.com>
2025-05-06 12:05:43 -0500

    tested out a adding method for the kills dictionary in here

--- test.py
+++ test.py
@@ -1,31 +1,34 @@
-class test():
-    def __init__(self):
-        self._ann = 8
-    
-    @property
-    def ann(self):
-        return self._ann
-    
-    @ann.setter
-    def ann(self, value):
-        self._ann = value
+class adkd():
+    def __init__(self, name, level, race):
+        self.name = name
+        self.race = race
+        self.level = level
+entry = adkd("cat", 5, 'cat')                                      
+#entry_structure = {entry.race: {'count': 1, 
+#                                'characters': {'level': {entry.level:{entry.name: {'information'}}}}}}
+                #the list       #the kind of enemy     #what level each was at # 
+#{what race:{how many, character: {level:{level:{name{information}}}}}}
+dic = {}
 
-class quesistion(test):
-    def __init__(self):
-        super().__init__()
-    
-    @property
-    def ann(self): 
-        return self._ann
-    
-    @ann.setter
-    def ann(self, value):
-        if isinstance(value, int):
-            self._ann = value
+def adding(entry):
+    global dic
+    if isinstance(entry, adkd):
+        if entry.race not in dic:
+            dic[entry.race] = {"count": 1, entry.name: entry.__dict__}
         else:
-            return TypeError
-
-dd = quesistion()
-
-dd.ann = 9
-print(dd.ann)
+            dic[entry.race]['count'] = dic[entry.race]['count'] + 1 
+            dic[entry.race][entry.name] = entry.__dict__
+    else:
+        print('ERROR: entry must be of class <adkd>')
+            
+            
+first = adkd("cat1", 5, 'cat')
+first2 = adkd("cat2", 5, 'cat')
+second = adkd("dog1", 2, 'dog')
+second2 = adkd("dog2", 2, 'dog')
+adding(first)
+adding(first2)
+adding(second)
+adding(second2)
+for key, value in dic.items():
+    print(key, value) 
