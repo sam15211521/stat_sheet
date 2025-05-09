6689faa965079730ce570a8b8c8985ccd1b36d45
sam15211521 <70865013+sam15211521@users.noreply.github.com>
2025-05-09 15:39:16 -0500

    moved the path to another area for simplicity sake

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import random
 import pickle
 #from characters.ben import ben
+cur_path = os.path.dirname(__file__)
+file_path = 'characters/ben.dat'
+abs_file_path = os.path.join(cur_path, file_path)
 
 def clearscreen():
     os.system('cls')
 ####################
 clearscreen()
-cur_path = os.path.dirname(__file__)
-file_path = 'characters/ben.dat'
-abs_file_path = os.path.join(cur_path, file_path)
 print("\n")
 
 with open(abs_file_path, 'rb') as file:
