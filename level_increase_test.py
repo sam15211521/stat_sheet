1bec59ded9ad03f25fd1de8292e539bfd44df16b
sam15211521 <70865013+sam15211521@users.noreply.github.com>
2025-05-15 13:14:01 -0500

    the window is not complete, while it will not be the final implementation, it is a good testing ground

--- level_increase_test.py
+++ level_increase_test.py
@@ -10,8 +10,8 @@ from PySide6.QtGui import *
 
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
+class stat_increase_window(QMainWindow):
+    def __init__(self, character : Character):
         super().__init__()
         #boiler plate logic
         self.central_widget = QWidget(self)
@@ -19,31 +19,47 @@ class MainWindow(QMainWindow):
         self.central_layout = QGridLayout()
         self.central_widget.setLayout(self.central_layout)
         
-        self.stat = Stat("Default")
+        self.character = character
 
         self.usable_mana = QDoubleSpinBox()
-        self.usable_mana.setValue(0)
+        
+        self.usable_mana.setValue(self.character.condensed_mana.level)
+        #self.usable_mana.lineEdit().setReadOnly(True)
         self.usable_mana.setSingleStep(1)
         self.usable_mana.setDecimals(0)
-        self.usable_mana.valueChanged.connect(self.print_value)
+        self.usable_mana.setMaximum(1000)
+        self.usable_mana.valueChanged.connect(self.set_con_mana)
 
-
-        self.text_mana_to_next_level = QLabel(f'Mana to next level: {self.stat.mana_to_next_level}')
-        self.text_total_mana = QLabel(f'Total Mana: {self.stat._total_mana_used}')
+        self.character_name =f"{self.character.name}: {self.character.magic_endurance.name} | {self.character.magic_endurance.level}"
+        self.character_name = QLabel(f"{self.character.name}: {self.character.magic_endurance.name} | {self.character.magic_endurance.level}")
+        self.text_mana_to_next_level = QLabel(f'Mana to next level: {self.character.magic_endurance.mana_to_next_level}')
+        self.text_total_mana = QLabel(f'Total Mana: {self.character.magic_endurance._total_mana_used}')
+        self.power_label = QLabel(f'Power: {self.character.magic_endurance.power}')
 
         self.increase_level_button = QPushButton("Increase Level")
+        self.increase_level_button.clicked.connect(self.increase_level)
 
-        self.central_layout.addWidget(self.usable_mana,0,0)
-        self.central_layout.addWidget(self.text_mana_to_next_level,1,0)
-        self.central_layout.addWidget(self.text_total_mana,2,0)
-        self.central_layout.addWidget(self.increase_level_button,3,0)
+        self.central_layout.addWidget(self.character_name,0,0)
+        self.central_layout.addWidget(self.usable_mana,1,0)
+        self.central_layout.addWidget(self.text_mana_to_next_level,2,0)
+        self.central_layout.addWidget(self.text_total_mana,3,0)
+        self.central_layout.addWidget(self.power_label, 4, 0)
+        self.central_layout.addWidget(self.increase_level_button,5,0)
 
-    def print_value(self):
-        print(self.usable_mana.value())
+    def set_con_mana(self):
+        self.character.condensed_mana.level = int(self.usable_mana.value())
+
     
     def increase_level(self):
-        current_requirment = self.stat.mana_to_next_level
-        pass
+        #current_requirment = self.character.mana_to_next_level
+        if self.character.condensed_mana.level >= self.character.magic_endurance.mana_to_next_level:
+            self.character.use_con_mana_to_increase_stat_level(self.character.magic_endurance)
+            self.usable_mana.setValue(self.character.condensed_mana.level)
+            self.character_name.setText(f"{self.character.name}: {self.character.magic_endurance.name} | {self.character.magic_endurance.level}")
+            self.text_total_mana.setText(f'Total Mana: {self.character.magic_endurance._total_mana_used}')
+            self.text_mana_to_next_level.setText(f'Mana to next level: {self.character.magic_endurance.mana_to_next_level}')
+            self.power_label.setText(f'Power: {self.character.magic_endurance.power}')
+
 
 #a = Stat("default")
 #current_level = a.mana_to_next_level
@@ -51,7 +67,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication()
-    window = MainWindow()
+    window = stat_increase_window(Character("Ben"))
     window.show()
 
     sys.exit(app.exec())
