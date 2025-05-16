75f7e3391d8e768c38b92834087a4a939b7fe669
Sam Hendrix <sam15211521jody@gmail.com>
2025-05-15 21:49:24 -0800

    put the save and load function in here for even easier access

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from characters.ben import ben
 from Character import Character
 from windows import MainWindow 
 from PySide6.QtWidgets import QApplication
-from Character import Character
-#from characters.ben import ben
 cur_path = os.path.dirname(__file__)
 file_path = 'characters/cat.dat'
 abs_file_path = os.path.join(cur_path, file_path)
@@ -38,18 +36,3 @@ def main():
 
 if __name__ == "__main__":
     save()
-clearscreen()
-print("\n")
-ben = Character("Ben",body_mana_multiplier=2722) #hiden stat = 15.327
-with open(abs_file_path,'wb') as file:
-    pickle.dump(ben, file)
-
-with open(abs_file_path, 'rb') as file:
-    loaded_data = pickle.load(file)
-
-print(loaded_data)
-
-
-
-print("\n")
-
