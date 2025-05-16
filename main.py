880aa4821838a741182924d0822ccd355c5501a7
Sam Hendrix <sam15211521jody@gmail.com>
2025-05-15 20:18:56 -0800

    not sure either

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import os
 import random
+from sys import exit as sys_exit
 import pickle
-#from characters.ben import ben
+from characters.ben import ben
+from Character import Character
+from windows import MainWindow 
+from PySide6.QtWidgets import QApplication
 cur_path = os.path.dirname(__file__)
-file_path = 'characters/ben.dat'
+file_path = 'characters/cat.dat'
 abs_file_path = os.path.join(cur_path, file_path)
 
 def clearscreen():
     os.system('cls')
 ####################
-clearscreen()
-print("\n")
 
-with open(abs_file_path, 'rb') as file:
-    loaded_data = pickle.load(file)
+info = Character
+def save():
+    with open(abs_file_path, 'wb') as File:
+        pickle.dump(ben, File)
 
-print(loaded_data)
+def load():
+    global info
+    with open(abs_file_path, "rb") as File:
+        info = pickle.load(File)
+        return info
+
+
+def main():
+    app = QApplication()
+    window = MainWindow('ex')
+    window.show()
+    sys_exit(app.exec())
 
 
 
-print("\n")
+if __name__ == "__main__":
+    save()
 
